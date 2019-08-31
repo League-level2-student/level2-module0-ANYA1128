dad771fc95e89a88b948ae94853e487e966b289b
league <league@imac-22.attlocal.net>
2019-08-30 18:28:49 -0700

    ArrayCheatSheet, RobotRace(IP) start next steps that are not completed.

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,36 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	static int a = 0;
 
-	//2. create an array of 5 robots.
+	public static void main(String[] args) {
+		Robot[] array = new Robot[5];
 
-	//3. use a for loop to initialize the robots.
+		for (int i = 0; i < array.length; i++) {
+			a = a + 150;
+			array[i] = new Robot();
+			array[i].setX(a);
+			array[i].setY(550);
+		}
+	}
+	// 2. create an array of 5 robots.
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+	// 3. use a for loop to initialize the robots.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+	// 4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+	// 5. use another for loop to iterate through the array and make each robot move
+	// a random amount less than 50.
 
-	//7. declare that robot the winner and throw it a party!
+	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+	// screen.
 
-	//8. try different races with different amounts of robots.
+	// 7. declare that robot the winner and throw it a party!
 
-	//9. make the robots race around a circular track.
+	// 8. try different races with different amounts of robots.
+
+	// 9. make the robots race around a circular track.
 }
